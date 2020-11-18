5efe1623e76c5abaa623552f0b7c7d2478ab7f9e
paul_abib <paulabib17@gmail.com>
2020-11-18 15:59:21 +0100

    calculate should return the addition of two last when given stack of multiple number

---
+++ rpn-core/src/main/java/io/pabib/rpn/operation/Substraction.java
@@ -0,0 +1,17 @@
+package io.pabib.rpn.operation;
+
+import java.util.Optional;
+import java.util.Stack;
+
+public class Substraction implements Operation{
+    @Override
+    public Optional<Double> calculate(Stack<Double> stack) {
+        if(stack == null)
+            return Optional.empty();
+        if(stack.size() < 2)
+            return Optional.empty();
+        Double second = stack.pop();
+        Double first = stack.pop();
+        return Optional.of(first - second);
+    }
+}
